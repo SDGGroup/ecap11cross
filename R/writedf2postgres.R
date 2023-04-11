1eb424b7e48dc8f14ddbdb680689f6604073fb43
emanuele-depaoli-sdg <emanuele.depaoli@sdggroup.com>
2023-04-11 18:46:54 +0200

    add con2bigquery object as argument

--- R/writedf2postgres.R
+++ R/writedf2postgres.R
@@ -1,14 +1,13 @@
 #' writedf2postgres
 #' @description Funzione per appendere un dataframe a una tabella del database `ddl-dcerm-db` su Postgres
+#' @param .con un oggetto output di \link{connect2postgres}, usato per comunicare con il database 
 #' @param .nome_tabella `chr` nome tabella di destinazione
 #' @param .dataframe `tibble` tibble da scrivere
-#' @returns `bool` TRUE, invisibilmente. 
+#' @returns `bool` TRUE, invisibilmente
 #' @export
 
-writedf2postgres <- function(.nome_tabella, .dataframe){
+writedf2postgres <- function(.con, .nome_tabella, .dataframe){
   
-  con <- connect2postgres()
-  result <- dbWriteTable(con, .nome_tabella, .dataframe, append = TRUE)
-  dbDisconnect(con)
+  dbWriteTable(.con, .nome_tabella, .dataframe, append = TRUE)
   
 }

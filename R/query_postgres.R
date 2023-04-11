67389881b706396b2447eab7f1aadf37b965dc92
emanuele-depaoli-sdg <emanuele.depaoli@sdggroup.com>
2023-04-11 18:47:30 +0200

    add con2bq object as argument

--- R/query_postgres.R
+++ R/query_postgres.R
@@ -1,14 +1,13 @@
 #' query_postgres
 #' @description Funzione che esegue una query data in input in Postgres
+#' @param .con un oggetto output di \link{connect2postgres}, usato per comunicare con il database 
 #' @param query `chr` query da eseguire su Postgres.
 #' @returns `tibble`
 #' @export
 
-query_postgres <- function(query){
+query_postgres <- function(.con, .query){
   
-  con <- connect2postgres()
-  result <- dbFetch( dbSendQuery(con, query) )
-  dbDisconnect(con)
+  result <- dbFetch( dbSendQuery(.con, .query) )
   
   return(tibble(result))
 }
